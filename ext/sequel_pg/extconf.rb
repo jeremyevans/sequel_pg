1ec92abd8f90b8189da0f1bd6bb1c4766edb3280
Jeremy Evans <code@jeremyevans.net>
2010-08-29 15:33:02 -0700

    Add a cross compiling gem and distclean task

--- ext/sequel_pg/extconf.rb
+++ ext/sequel_pg/extconf.rb
@@ -2,6 +2,13 @@ require 'mkmf'
 $CFLAGS << " -Wall " unless RUBY_PLATFORM =~ /solaris/
 dir_config('pg', ENV["POSTGRES_INCLUDE"] || (IO.popen("pg_config --includedir").readline.chomp rescue nil),
                  ENV["POSTGRES_LIB"]     || (IO.popen("pg_config --libdir").readline.chomp rescue nil))
+
+if enable_config("static-build")
+	# Link against all required libraries for static build, if they are available
+	have_library('gdi32', 'CreateDC') && append_library($libs, 'gdi32')
+	have_library('secur32') && append_library($libs, 'secur32')
+end
+
 if (have_library('pq') || have_library('libpq') || have_library('ms/libpq')) && have_header('libpq-fe.h')
   create_makefile("sequel_pg")
 else
