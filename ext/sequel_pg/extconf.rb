fea35f51a42a6348d6d6cacb366550d2e303a7cd
Jeremy Evans <code@jeremyevans.net>
2017-10-16 16:02:18 -0700

    Remove static-build from extconf
    This was used for building the Windows gems, but that hasn't been
    supported for while.
    
    Remove the -ggdb from DEBUG while here.

--- ext/sequel_pg/extconf.rb
+++ ext/sequel_pg/extconf.rb
@@ -1,17 +1,8 @@
 require 'mkmf'
-$CFLAGS << " -O0 -g -ggdb" if ENV['DEBUG']
+$CFLAGS << " -O0 -g" if ENV['DEBUG']
 $CFLAGS << " -Wall " unless RUBY_PLATFORM =~ /solaris/
 dir_config('pg', ENV["POSTGRES_INCLUDE"] || (IO.popen("pg_config --includedir").readline.chomp rescue nil),
                  ENV["POSTGRES_LIB"]     || (IO.popen("pg_config --libdir").readline.chomp rescue nil))
-
-if enable_config("static-build")
-	# Link against all required libraries for static build, if they are available
-	have_library('gdi32', 'CreateDC')
-	have_library('secur32')
-	have_library('ws2_32')
-	have_library('eay32')
-	have_library('ssleay32', 'SSL_pending')
-end
 
 if (have_library('pq') || have_library('libpq') || have_library('ms/libpq')) && have_header('libpq-fe.h')
   have_func 'PQsetSingleRowMode'
