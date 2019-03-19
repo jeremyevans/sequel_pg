529c210acc0c9e8586d8eff1788c72187ad884a3
Jeremy Evans <code@jeremyevans.net>
2019-03-19 11:35:47 -0700

    Use pkg_config instead of pg_config during extconf
    Except if POSTGRES_INCLUDE or POSTGRES_LIB environment variables
    are used, indicating the user wants to do manual customization.

--- ext/sequel_pg/extconf.rb
+++ ext/sequel_pg/extconf.rb
@@ -1,8 +1,11 @@
 require 'mkmf'
 $CFLAGS << " -O0 -g" if ENV['DEBUG']
 $CFLAGS << " -Wall " unless RUBY_PLATFORM =~ /solaris/
+
+if ENV["POSTGRES_INCLUDE"] || ENV["POSTGRES_LIB"] || !pkg_config('libpq')
 dir_config('pg', ENV["POSTGRES_INCLUDE"] || (IO.popen("pg_config --includedir").readline.chomp rescue nil),
                  ENV["POSTGRES_LIB"]     || (IO.popen("pg_config --libdir").readline.chomp rescue nil))
+end
 
 if (have_library('pq') || have_library('libpq') || have_library('ms/libpq')) && have_header('libpq-fe.h')
   have_func 'PQsetSingleRowMode'
