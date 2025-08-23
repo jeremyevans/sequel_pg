84970092095cb9dc97321128afb961aed9fbd7c0
Jeremy Evans <code@jeremyevans.net>
2025-08-22 17:33:26 -0700

    Use SimpleCov.add_filter block instead of string
    Using a string is almost always a bad idea, because if any part of
    the path contains the string, it will filter out the path. Switch
    to using a block and doing a match on the entire path prefix.

--- spec/coverage_helper.rb
+++ spec/coverage_helper.rb
@@ -4,7 +4,7 @@ SimpleCov.start do
   enable_coverage :branch
   command_name ENV['SIMPLECOV_COMMAND_NAME']
   root File.dirname(__dir__)
-  add_filter "/spec/"
+  add_filter{|f| f.filename.match(%r{\A#{Regexp.escape(File.dirname(__FILE__))}/})}
   add_group('Missing'){|src| src.covered_percent < 100}
   add_group('Covered'){|src| src.covered_percent == 100}
 end
